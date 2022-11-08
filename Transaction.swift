014803a18ef5bb4b0168435bf5c086fba3c2dde9
vishnumvk <mvishnukumar.mk@gmail.com>
2022-11-08 17:54:55 +0530

    added formatting

--- Transaction.swift
+++ Transaction.swift
@@ -9,12 +9,14 @@ import Foundation
 
 enum TransactionType: String,Codable{
     case credit
-    case debit = "debit "
+    case debit
 }
 
 struct Transaction: CustomStringConvertible,Codable{
     var description: String{
-        return "TID: \(self.tID)  \(self.date)   credit/debit: \(self.type.rawValue)  amount: \(String(format: "%10.2f", self.amount))    by: \(self.by)"
+        var type = self.type.rawValue
+        let paddedType = type.padding(toLength: 8, withPad: " ", startingAt: 0)
+        return "TID: \(self.tID)  \(self.date)   credit/debit: \(paddedType) amount:\(String(format: "%10.2f", self.amount))  by: \(self.by)"
     }
     
     let tID: String
